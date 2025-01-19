72353461a9d449249fd5f7b723d6cd2e361f56d1
tayoky <stanislas343@gmail.com>
2025-01-19 19:48:32 +0100

    add tar header

---
+++ kernel/fs/include/tarfs.h
@@ -0,0 +1,35 @@
+#ifndef TARFS_H
+#define TARFS_H
+#include <stdint.h>
+
+typedef struct ustar_header_struct{
+	char name[100];
+	char file_mode[8];
+	char owner[8];
+	char group[8];
+	char file_size[12];
+	char last_modif[12];
+	uint64_t checksum;
+	char type;
+	char linked_file[100];
+	char ustart[6];
+	char owner_name[32];
+	char group_name[32];
+	uint64_t major_number;
+	uint64_t minor_number;
+	char prefix[155];
+}ustar_header;
+
+#define USTAR_REGTYPE  '0'        // Regular file (preferred code)
+#define USTAR_AREGTYPE '\0'       // Regular file (alternate code)
+#define USTAR_LNKTYPE  '1'        // Hard link
+#define USTAR_SYMTYPE  '2'        // Symbolic link (hard if not supported)
+#define USTAR_CHRTYPE  '3'        // Character special
+#define USTAR_BLKTYPE  '4'        // Block special
+#define USTAR_DIRTYPE  '5'        // Directory
+#define USTAR_FIFOTYPE '6'        // Named pipe
+#define USTAR_CONTTYPE '7'        // Contiguous file
+
+void mount_initrd(void);
+
+#endif
