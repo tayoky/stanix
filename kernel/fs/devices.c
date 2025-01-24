404e7dfa5322dae2a2f18e47e02c02af8796321b
tayoky <tayoky4848@gmail.com>
2025-01-24 21:11:27 +0100

    add dev:/zero

--- kernel/fs/devices.c
+++ kernel/fs/devices.c
@@ -6,11 +6,25 @@
 #include "asm.h"
 #include "string.h"
 
+uint64_t zero_read(vfs_node *node,const void *buffer,uint64_t offset,size_t count){
+	memset(buffer,0,count);
+	return count;
+}
+
+device_op zero_op = {
+	.read = zero_read
+};
+
 void init_devices(void){
 	kstatus("init dev ...");
 	if(vfs_mount("dev",new_tmpfs())){
 		kfail();
 		kinfof("fail to mount devfs on dev:/\n");
+		halt();
+	}
+	if(vfs_create_dev("dev:/zero",&zero_op,NULL)){
+		kfail();
+		kinfof("fail to create device dev:/zero\n");
 		halt();
 	}
 	kok();
