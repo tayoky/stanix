d268453b4dd33f35b495f1d920421e7617b0f770
tayoky <tayoky4848@gmail.com>
2025-01-26 20:08:26 +0100

    new header for framebuffer

--- kernel/drivers/include/framebuffer.h
+++ kernel/drivers/include/framebuffer.h
@@ -7,7 +7,14 @@ void init_frambuffer(void);
 
 uint64_t framebuffer_write(vfs_node *node,void *buffer,uint64_t offset,size_t count);
 int framebuffer_ioctl(vfs_node *node,uint64_t request,void *arg);
-
+void draw_pixel(vfs_node *framebuffer,uint64_t x,uint64_t y,uint32_t color);
 #define IOCTL_FRAMEBUFFER_WIDTH  0x01
 #define IOCTL_FRAMEBUFFER_HEIGHT 0x02
+#define IOCTL_FRAMEBUFFER_BPP    0x03
+#define IOCTL_FRAMEBUFFER_RM     0x04
+#define IOCTL_FRAMEBUFFER_RS     0x05
+#define IOCTL_FRAMEBUFFER_GM     0x06
+#define IOCTL_FRAMEBUFFER_GS     0x07
+#define IOCTL_FRAMEBUFFER_BM     0x08
+#define IOCTL_FRAMEBUFFER_BS     0x09
 #endif
