b072741f92c9ebe12607c5dd0f91c918e053b5b6
tayoky <tayoky4848@gmail.com>
2025-01-29 20:01:33 +0100

    add license

--- kernel/main.c
+++ kernel/main.c
@@ -34,12 +34,28 @@ static void ls(const char *path){
 	vfs_close(node);
 }
 
+void print_license(void){
+        kinfof("the STANIX kernel\n"
+                "Copyright (C) 2025  tayoky\n"
+                "\n"
+                "This program is free software: you can redistribute it and/or modify\n"
+                "it under the terms of the GNU General Public License as published by\n"
+                "the Free Software Foundation, either version 3 of the License, or\n"
+                "(at your option) any later version.\n"
+                "\n"
+                "This program is distributed in the hope that it will be useful,\n"
+                "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                "GNU General Public License for more details.\n");
+}
+
 //the entry point
 void kmain(){
         kernel = &master_kernel_table;
         disable_interrupt();
         init_serial();
         kinfof("starting stanix kernel\n");
+        print_license();
         get_bootinfo();
         init_gdt();
         init_idt();
