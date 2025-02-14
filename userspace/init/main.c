97a237504361f9bb86e4fa0a24400ec2dee50b72
tayoky <tayoky4848@gmail.com>
2025-02-14 19:57:45 +0100

    init : use printf instead of snprintf

--- userspace/init/main.c
+++ userspace/init/main.c
@@ -2,14 +2,21 @@
 #include <syscall.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void __tlibc_init();
 
 int main(){
+	__tlibc_init();
+
 	//init std streams
 	open("dev:/null",O_RDONLY); //stdin
 	open("dev:/tty0",O_WRONLY); //stdout
 	open("dev:/tty0",O_WRONLY); //stderr
 
-	char hello[] = "hello world !!\n";
-	write(STDOUT_FILENO,hello,strlen(hello));
+	printf("hello world !!\n");
+
 	return 0;
 }
+
