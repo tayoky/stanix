7f93ab59959c910baade55322795852e0923d0c1
tayoky <tayoky4848@gmail.com>
2025-02-17 19:24:42 +0100

    full cmos driver

--- kernel/drivers/cmos.c
+++ kernel/drivers/cmos.c
@@ -42,6 +42,22 @@ uint8_t read_cmos(uint8_t address){
 	return data;
 }
 
+extern struct timeval time;
+
+static int is_leap(uint64_t year){
+	//not divisible by 4
+	if(year % 4){
+		return 0;
+	}
+
+	//excecptioh for mutiple of 100 tha can't be divide by 400
+	if((year % 400) && !(year % 100)){
+		return 0;
+	}
+
+	return 1;
+}
+
 void init_cmos(void){
 	kstatus("start reading cmos... ");
 	cmos_wait();
@@ -49,11 +65,71 @@ void init_cmos(void){
 	uint8_t seconds = read_cmos(0x00);
 	uint8_t minutes = read_cmos(0x02);
 	uint8_t hours   = read_cmos(0x04);
-	uint8_t day     = read_cmos(0x07);
+	uint64_t day    = read_cmos(0x07);
 	uint8_t month   = read_cmos(0x08);
-	uint8_t years   = read_cmos(0x09);
+	uint64_t years  = read_cmos(0x09);
 
+	//remove today day
+	day--;
+
+	if(years > 90){
+		years += 1900;
+	} else {
+		years += 2000;
+	}
+
+	switch (month)
+	{
+	case 12:
+		day += 30;
+	case 11:
+		day += 31;
+	case 10:
+		day += 30;
+	case 9:
+		day += 31;
+	case 8:
+		day += 31;
+	case 7:
+		day += 30;
+	case 6:
+		day += 31;
+	case 5:
+		day += 30;
+	case 4:
+		day += 31;
+	case 3:
+		if(is_leap(years)){
+			day += 29;
+		} else
+		{
+			day += 28;
+		}
+	case 2:
+		day += 31;
+	case 1:
+		break;
 	
+	default:
+		kfail();
+		kinfof("can't read : month is %d\n",month);
+		return;
+		break;
+	}
 
+	//now convert years in day
+	while(years > 1970){
+		years--;
+		if(is_leap(years)){
+			day += 366;
+		} else {
+			day += 365;
+		}
+		
+	}
+
+	time.tv_sec = seconds + minutes * 60 + hours * 3600 + day * 86400;
 	kok();
+
+	kinfof("current time : %ld\n",time.tv_sec);
 }
