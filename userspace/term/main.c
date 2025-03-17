a076cdd5c36d15be569945a37f43664d7cc1cd30
tayoky <tayoky4848@gmail.com>
2025-03-17 18:40:40 +0100

    most basic terminal emulator

---
+++ userspace/term/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <input.h>
+#include <fcntl.h>
+
+//most basic terminal emumator
+int main(int argc,char **argv){
+	printf("starting userspace terminal emulator\n");
+	char pipefd[2];
+	pipe(pipefd);
+
+	dup2(pipefd[0],STDIN_FILENO);
+
+	//try open keayboard
+	int kbd_fd = open("dev:/kb0",O_RDONLY);
+
+	if(kbd_fd < 0){
+		perror("dev:/kb0");
+		exit(1);
+	}
+
+	pid_t child = fork();
+	if(!child){
+		//launch tsh because no login
+		char *arg[] = {
+			"initrd:/bin/tsh",
+			NULL
+		};
+		execv(arg[0],arg);
+		perror("initrd:/bin/tsh");
+	}
+
+	for(;;){
+		struct input_event event;
+
+		if(read(kbd_fd,&event,sizeof(event)) < 1){
+			continue;
+		}
+
+		//ignore not key event
+		if(event.ie_type != IE_KEY_EVENT){
+			continue;
+		}
+
+		//ignore key release
+		if(event.ie_key.flags & IE_KEY_RELEASE){
+			continue;
+		}
+
+		//put into the pipe
+		if(event.ie_key.flags & IE_KEY_GRAPH){
+			write(kbd_fd,&event.ie_key.c,1);
+			continue;
+		}
+		
+	}
+
+}
