fde8b0e06db8141b1afb30e1cff064a8ab7fec2d
tayoky <tayoky4848@gmail.com>
2025-03-29 22:25:37 +0100

    aarch64 : most basic kernel panic

---
+++ kernel/arch/aarch64/panic.c
@@ -0,0 +1,21 @@
+#include "panic.h"
+#include "print.h"
+#include "asm.h"
+#include "serial.h"
+
+int panic_count = 0;
+
+void panic(const char *error,fault_frame *fault){
+	panic_count ++;
+	if(panic_count > 2){
+		halt();
+	}
+	if(panic_count > 1){
+		write_serial("kernel panic panicked\n");
+		halt();
+	}
+	disable_interrupt();
+
+	kprintf("KERNEL PANIC\n");
+	halt();
+}
