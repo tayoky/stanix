047eff879f033efe8ea927535711edf6804b0876
tayoky <tayoky4848@gmail.com>
2025-03-31 18:24:28 +0200

    update aarch64 launch script

--- aarch.sh
+++ aarch.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-qemu-system-aarch64 -cpu cortex-a76 -M virt -m 512m -usb -device qemu-xhci -device usb-kbd -device usb-mouse -device ramfb -device ahci,id=ahci -device ide-hd,drive=hdd0 -drive if=pflash,unit=0,format=raw,file=OVMF.fd -drive file=stanix.iso,if=none,id=hdd0
+make OVMF-img.bin
+qemu-system-aarch64 -cpu cortex-a76 -M virt -m 512m -usb -device qemu-xhci -device usb-kbd -device usb-mouse -device ramfb -device ahci,id=ahci -drive if=pflash,unit=0,format=raw,file=OVMF-img.bin -cdrom stanix.iso
