508fb2eed5b854559af8aefe6b8880d94b7504ad
tayoky <tayoky4848@gmail.com>
2025-05-18 19:47:04 +0200

    signal : userspace test signal program

---
+++ userspace/test-sig/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <signal.h>
+
+volatile int flag = 0;
+
+void handler(){
+	printf("SIGUSR1\n");
+	flag = 1;
+	return;
+}
+
+int main(){
+	if(signal(SIGUSR1,handler) == SIG_ERR){
+		perror("signal");
+		return 1;
+	}
+
+	//first just test sending the signal
+	flag = 0;
+	if(raise(SIGUSR1) < 0){
+		perror("raise");
+		return 1;
+	}
+	
+	//spin until flags is set
+	while(!flag);
+	printf("raise and signal test succed\n");
+
+	sigset_t usr1;
+	sigemptyset(&usr1);
+	sigaddset(&usr1,SIGUSR1);
+	if(sigprocmask(SIG_BLOCK,&usr1,NULL) < 0){
+		perror("sigprocmask");
+		return 1;
+	}
+
+	flag = 0;
+
+	if(raise(SIGUSR1) < 0){
+		perror("raise");
+		return 1;
+	}
+
+	//now unblock and the signal should be triggerd
+	if(sigprocmask(SIG_UNBLOCK,&usr1,NULL) < 0){
+		perror("sigprocmask");
+		return 1;
+	}
+	while(!flag);
+	printf("block/unblock test succed\n");
+
+	return 0;
+}
