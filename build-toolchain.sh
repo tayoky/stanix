063e1058a06e68a9f1af1d0d9b8751fd24188e56
tayoky <tayoky4848@gmail.com>
2025-06-02 20:40:16 +0200

    build-toolchain : build gcc 12.2. 0 instead of gcc 15.1.0 build gcc 12.2.0 cause it's smaller and has all the feature we need

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -79,7 +79,7 @@ fi
 
 
 BINUTILS_VERSION=2.44
-GCC_VERSION=15.1.0
+GCC_VERSION=12.2.0
 if [ ! -e binutils.tar.xz ] ; then
   $WGET -Obinutils.tar.xz "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz"
 fi
