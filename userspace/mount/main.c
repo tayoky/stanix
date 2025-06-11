a5909af65e6053db3bd6e50c1205e4055ca1a3bd
tayoky <tayoky4848@gmail.com>
2025-06-11 19:24:51 +0200

    mount : --help option

--- userspace/mount/main.c
+++ userspace/mount/main.c
@@ -1,27 +1,51 @@
 #include <sys/mount.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+void help(){
+	printf("mount -t TYPE [-s] SOURCE [-T] TARGET\n");
+	printf("-t/--type   : precise type\n");
+	printf("-s/--source : precise source/device to mount (can be a stub for tmpfs)\n");
+	printf("-T/--target : path to mount to\n");
+}
 
 int main(int argc,char **argv){
 	char *type = NULL;
 	char *source = NULL;
 	char *target = NULL;
 
-	for (int i = 1; i < argc - 1; i++){
+	for (int i = 1; i < argc; i++){
 		if(!(strcmp(argv[i],"--target") && strcmp(argv[i],"-T"))){
 			i++;
+			if(!argv[i]){
+				fprintf(stderr,"expect path after --target\n");
+				return 1;
+			}
 			target = argv[i];
 			continue;
 		}
 		if(!(strcmp(argv[i],"--source") && strcmp(argv[i],"-S"))){
 			i++;
+			if(!argv[i]){
+				fprintf(stderr,"expect path after --source\n");
+				return 1;
+			}
 			source = argv[i];
 			continue;
 		}
 		if(!(strcmp(argv[i],"--type") && strcmp(argv[i],"-t"))){
 			i++;
+			if(!argv[i]){
+				fprintf(stderr,"expect type after --type\n");
+				return 1;
+			}
 			type = argv[i];
 			continue;
+		}
+		if(!strcmp(argv[i],"--help")){
+			help();
+			return 0;
 		}
 		if(!source){
 			source = argv[i];
