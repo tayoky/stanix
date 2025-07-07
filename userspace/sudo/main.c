8cbfc3ec23658acfd52534c14b5d3f2ba9f570ff
tayoky <tayoky4848@gmail.com>
2025-07-07 18:45:22 +0200

    sudo : functionnal sudo withoot password

--- userspace/sudo/main.c
+++ userspace/sudo/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 int main(int argc,char **argv){
@@ -7,7 +9,36 @@ int main(int argc,char **argv){
 		printf("setuid bit is not set on sudo\n");
 	}
 
-	printf("TODO xD\n");
+	if(argc < 2){
+		fprintf(stderr,"missing argument\n");
+		return 1;
+	}
+
+	if(!strcmp(argv[1],"--shell") || !strcmp(argv[1],"-s")){
+		char *shell = getenv("SHELL");
+		if(!shell){
+			shell = "sh";
+		}
+
+		const char *args[] = {
+			shell,
+			NULL
+		};
+
+		execvp(args[0],args);
+		perror(args[0]);
+		return 1;
+	}
+	if(!strcmp(argv[1],"--help")){
+		printf("usage : sudo COMMAND or\n");
+		printf("sudo OPTION\n");
+		printf("run a command as root\n");
+		printf("-s --shell : run a shell instead of a command\n");
+		return 0;
+	}
+
+	execvp(argv[1],(const char *const*)&argv[1]);
+	perror(argv[1]);
 
 	return 0;
 }
