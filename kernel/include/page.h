17c13e1ce8b73d7a6ca4c95b97de720fc6626476
tayoky <tayoky4848@gmail.com>
2025-07-10 18:08:35 +0200

    kernel : get PAGE_SIZE from limits.h

--- kernel/include/page.h
+++ kernel/include/page.h
@@ -1,7 +1,7 @@
 #ifndef PAGE_H
 #define PAGE_H
 
-#define PAGE_SIZE 0x1000
+#include <limits.h> //to get PAGE_SIZE
 
 #define PAGE_ALIGN_DOWN(address) (address/PAGE_SIZE * PAGE_SIZE)
 #define PAGE_ALIGN_UP(address)  ((address + PAGE_SIZE -1)/PAGE_SIZE * PAGE_SIZE)
