014a886c41581a899d6b514e388ff8e6c3d75657
tayoky <tayoky4848@gmail.com>
2025-07-27 18:59:41 +0200

    insmod : automaticly add /mod/ prefix if needed

--- userspace/insmod/main.c
+++ userspace/insmod/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/module.h>
 
 int main(int argc,const char **argv){
@@ -10,6 +11,12 @@ int main(int argc,const char **argv){
 	if(!strcmp("--help",argv[1])){
 		printf("usage : insmod MODULE [ARGUMENTS]\n");
 		return 0;
+	}
+
+	if(!strchr(argv[1],'/')){
+		char *path = malloc(strlen(argv[1]) + strlen("/mod/") + 1);
+		sprintf(path,"/mod/%s",argv[1]);
+		argv[1] = path;
 	}
 
 	if(insmod(argv[1],&argv[1])){
