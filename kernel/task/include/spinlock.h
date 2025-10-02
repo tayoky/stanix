43fd9b1455e966d7a91f3495038efa0dfe3db7c8
tayoky <tayoky4848@gmail.com>
2025-10-02 19:58:08 +0200

    spinlock : spinlock debug

--- kernel/task/include/spinlock.h
+++ kernel/task/include/spinlock.h
@@ -9,7 +9,15 @@ typedef struct spinlock {
     int had_interrupt;
 } spinlock;
 
-#define spinlock_acquire(l) {while(atomic_flag_test_and_set_explicit(&(l)->lock,memory_order_acquire));(l)->had_interrupt = have_interrupt();disable_interrupt();}
-#define spinlock_release(l) {atomic_flag_clear_explicit(&(l)->lock,memory_order_release); if((l)->had_interrupt)enable_interrupt();}
+#define SPINLOCK_DEBUG
+
+#ifdef SPINLOCK_DEBUG
+#include <kernel/print.h>
+#define spinlock_debug(...) kdebugf(__VA_ARGS__);
+#else
+#define spinlock_debug(...)
+#endif
+#define spinlock_acquire(l) {while(atomic_flag_test_and_set_explicit(&(l)->lock,memory_order_acquire));(l)->had_interrupt = have_interrupt();disable_interrupt();spinlock_debug("acquire spinlock %s\n",#l)}
+#define spinlock_release(l) {atomic_flag_clear_explicit(&(l)->lock,memory_order_release);spinlock_debug("release spinlock %s\n",#l) if((l)->had_interrupt)enable_interrupt();}
 
 #endif
