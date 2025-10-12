3d362194fc93b79890ea859435f2aa3879447a3a
tayoky <tayoky4848@gmail.com>
2025-10-12 18:55:21 +0200

    test-pthread : wait for thread to die

--- userspace/test-pthread/main.c
+++ userspace/test-pthread/main.c
@@ -2,11 +2,16 @@
 #include <pthread.h>
 #include <stddef.h>
 
+volatile int flag = 0;
+
 void *test(void *arg){
 	puts("hello\n");
+	flag = 1;
 }
 
 int main(){
 	pthread_t thread;
 	pthread_create(&thread,NULL,test,NULL);
+	while(!flag);
+	return 0;
 }
