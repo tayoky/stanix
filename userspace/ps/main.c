dcdbe019c9e13376c91fb7864e871f45ebb4cd28
tayoky <tayoky4848@gmail.com>
2026-02-06 19:24:01 +0100

    ps : show cmdline

--- userspace/ps/main.c
+++ userspace/ps/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <limits.h>
-#include <pwd.h>
 #include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <pwd.h>
 
 #define SELECT_CLASSIC   0
 #define SELECT_NLEAD_TTY 1 //all wth attached tty
@@ -13,48 +16,65 @@
 int ret = 0;
 int select = SELECT_CLASSIC;
 
-char *uid2name(uid_t uid){
+char *uid2name(uid_t uid) {
 	struct passwd *pwd = getpwuid(uid);
-	if(!pwd){
+	if (!pwd) {
 		static char buf[64];
-		sprintf(buf,"%ld",uid);
+		sprintf(buf, "%ld", uid);
 		return buf;
 	}
 	return pwd->pw_name;
 }
 
-void print(const char *path,const char *pid){
+void print(const char *path, const char *pid) {
 	struct stat st;
-	if(stat(path,&st) < 0){
+	if (stat(path, &st) < 0) {
 		perror(path);
 		ret = 1;
 		return;
 	}
-	printf("%-10s %5s\n",uid2name(st.st_uid),pid);
+	char cmdline[256];
+	char cmdline_path[256];
+	snprintf(cmdline_path, sizeof(cmdline_path), "%s/cmdline", path);
+	int cmdline_fd = open(cmdline_path, O_RDONLY);
+	if (cmdline_fd >= 0) {
+		ssize_t size = read(cmdline_fd, cmdline, sizeof(cmdline) - 1);
+		if (size < 0) {
+			perror(cmdline_path);
+			ret = 1;
+			close(cmdline_fd);
+		}
+		cmdline[size] = '\0';
+		close(cmdline_fd);
+	} else {
+		// on some olders stanix versions, cmdline does not exist
+		strcpy(cmdline, "unknow");
+	}
+	printf("%-10s %5s %s\n", uid2name(st.st_uid), pid, cmdline);
 }
 
 
-int main(int argc,char **argv){
-	for(int i=1; i<argc; i++){
-		if(argv[i][0] != '-'){
-			fprintf(stderr,"ps : invalid option '%s'\n",argv[i]);
+int main(int argc, char **argv) {
+	for (int i=1; i < argc; i++) {
+		if (argv[i][0] != '-') {
+			fprintf(stderr, "ps : invalid option '%s'\n", argv[i]);
 			return 1;
 		}
 	}
 	DIR *proc = opendir("/proc");
-	if(!proc){
+	if (!proc) {
 		perror("/proc");
 		return 1;
 	}
 
-	for(;;){
+	for (;;) {
 		struct dirent *ent = readdir(proc);
-		if(!ent)break;
-		if(!isdigit(ent->d_name[0]))continue;
+		if (!ent)break;
+		if (!isdigit(ent->d_name[0]))continue;
 
 		char path[PATH_MAX + 6];
-		snprintf(path,sizeof(path),"/proc/%s",ent->d_name);
-		print(path,ent->d_name);
+		snprintf(path, sizeof(path), "/proc/%s", ent->d_name);
+		print(path, ent->d_name);
 	}
 
 	closedir(proc);
