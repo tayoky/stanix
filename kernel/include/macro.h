6ccf3edb6530cf3979eb122e9bd96113545cdd58
tayoky <tayoky4848@gmail.com>
2026-02-06 18:26:55 +0100

    kernel : spinlock : more info in spinlock debugger

---
+++ kernel/include/macro.h
@@ -0,0 +1,9 @@
+#ifndef _KERNEL_MACRO_H
+#define _KERNEL_MACRO_H
+
+// a bunch of macro utils
+
+#define STRINGIFY(a) _STRINGIFY(a)
+#define _STRINGIFY(a) #a
+
+#endif
--- kernel/include/task/spinlock.h
+++ kernel/include/task/spinlock.h
@@ -6,6 +6,8 @@
 
 #ifdef SPINLOCK_DEBUG
 #include <kernel/panic.h>
+#include <kernel/macro.h>
+#include <kernel/print.h>
 #endif
 #include <kernel/asm.h>
 #include <stdatomic.h>
@@ -13,18 +15,30 @@
 typedef struct spinlock {
     atomic_flag lock;
     int had_interrupt;
+#ifdef SPINLOCK_DEBUG
+    const char *line;
+#endif
 } spinlock_t;
 
 
+#ifdef SPINLOCK_DEBUG
+static inline void __spinlock_acquire(spinlock_t *lock, const char *line) {
+    lock->had_interrupt = have_interrupt();
+    disable_interrupt();
+    while (atomic_flag_test_and_set_explicit(&lock->lock, memory_order_acquire)){
+        kdebugf("deadlock, lock acquired at %s\n", lock->line);
+        panic("deadlock", NULL);
+    }
+    lock->line = line;
+}
+#define spinlock_acquire(lock) __spinlock_acquire(lock, __FILE__ ":" STRINGIFY(__LINE__))
+#else
 static inline void spinlock_acquire(spinlock_t *lock) {
     lock->had_interrupt = have_interrupt();
     disable_interrupt();
-    while (atomic_flag_test_and_set_explicit(&lock->lock, memory_order_acquire))
-#ifdef SPINLOCK_DEBUG
-    panic("deadlock", NULL)
-#endif
-    ;
+    while (atomic_flag_test_and_set_explicit(&lock->lock, memory_order_acquire));
 }
+#endif
 
 static inline void spinlock_release(spinlock_t *lock) {
     atomic_flag_clear_explicit(&lock->lock, memory_order_release);
