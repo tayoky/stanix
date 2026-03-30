028b04188153efacfda6cbad2c1d10502e0cd4da
tayoky <tayoky4848@gmail.com>
2026-03-30 18:18:57 +0200

    stanix-version : basic stanix version util

---
+++ userspace/stanix-version/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <tgui/tgui.h>
+
+void add_label(tgui_box_t *box, const char *text) {
+	tgui_label_t *label = tgui_label_new(text);
+	tgui_widget_set_hexpand(TGUI_WIDGET_CAST(label), TGUI_TRUE);
+	tgui_box_append_widget(box, TGUI_WIDGET_CAST(label));
+}
+
+int main() {
+	if (tgui_init() < 0) {
+		puts("cannot init tgui");
+		return 1;
+	}
+	tgui_window_t *window = tgui_window_new("stanix version", 640, 480);
+	if (!window) {
+		return 1;
+	}
+	tgui_box_t *box = tgui_box_new();
+	tgui_widget_set_hexpand(TGUI_WIDGET_CAST(box), TGUI_TRUE);
+	tgui_widget_set_vexpand(TGUI_WIDGET_CAST(box), TGUI_TRUE);
+	tgui_widget_set_padding(TGUI_WIDGET_CAST(box), 5);
+	tgui_window_set_child(window, TGUI_WIDGET_CAST(box));
+	
+	
+	add_label(box, "The Stanix operating system");
+	add_label(box, "Copyright Tayoky 2024-2026 (GPL3)");
+	
+	tgui_main();
+	
+	tgui_widget_destroy(TGUI_WIDGET_CAST(window));
+	return 0;
+}
