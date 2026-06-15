2c187a2e9e50efb32b966ef51b02dfa7f52f84bb
tayoky <tayoky4848@gmail.com>
2026-06-15 20:51:55 +0200

    kernel : assert : improve assert message

--- kernel/include/assert.h
+++ kernel/include/assert.h
@@ -1,11 +1,12 @@
 #ifndef KERNEL_ASSERT_H
 #define _KERNEM_ASSERT_H
 
-//#define KASSERT_DEBUG
+#define KASSERT_DEBUG
 
 #ifdef KASSERT_DEBUG
 #include <kernel/panic.h>
-#define kassert(cond) if (!(cond)) {panic("assert : '" #cond "' failed", NULL);}
+#include <kernel/macro.h>
+#define kassert(cond) if (!(cond)) {panic(__FILE__ ":" STRINGIFY(__LINE__) " assert : '" #cond "' failed", NULL);}
 #else
 #define kassert(cond)
 #endif
