192f6daece721543039ada914e44825e2366ac24
tayoky <tayoky4848@gmail.com>
2026-08-06 21:44:36 +0200

    ata : ata : replace unsafe strcpy with safe copy

--- modules/block/ata/ata/ata.c
+++ modules/block/ata/ata/ata.c
@@ -1,3 +1,4 @@
+#include <kernel/userspace.h>
 #include <kernel/module.h>
 #include <kernel/block.h>
 #include <kernel/kheap.h>
@@ -71,9 +72,7 @@ static int ata_ioctl(block_device_t *block_device, long req, void *arg) {
 	ata_disk_t *disk = container_of(block_device, ata_disk_t, block_device);
 	switch (req) {
 	case I_MODEL:
-		// UNSAFE
-		strcpy(arg, disk->common_ident.model);
-		return 0;
+		return safe_copy_auto_to(arg, disk->common_ident.model);
 	default:
 		return -EINVAL;
 	}
