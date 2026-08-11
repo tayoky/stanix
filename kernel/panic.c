222fd2ed645421c33e207d1dfee7e162650f013b
tayoky <tayoky4848@gmail.com>
2026-08-11 09:01:44 +0200

    kernel : panic : move panic handler out of arch specific

--- kernel/panic.c
+++ kernel/panic.c
@@ -7,34 +7,31 @@
 #include <kernel/scheduler.h>
 #include <kernel/sym.h>
 
-int panic_count = 0;
-
-extern uint64_t p_kernel_end[];
+static atomic_int panic_count = 0;
 
 void panic(const char *error, registers_t *fault) {
 	disable_interrupt();
-	panic_count++;
-	if (panic_count > 1) {
-		earlycon_output_str("\nkernel panic paniced\n");
+	if (atomic_fetch_add(&panic_count) > 0) {
+		earlycon_output_str("\nkernel panic panicked\n");
 		halt();
 	}
-	pid_t pid           = 0;
+	pid_t tid           = 0;
 	uintptr_t stack_top = 0;
 	if (get_current_task()) {
-		pid       = get_current_task()->tid;
+		tid       = get_current_task()->tid;
 		stack_top = KSTACK_TOP(get_current_task()->kernel_stack);
 	}
 	kprintf(COLOR_RED "================= ERROR : KERNEL PANIC =================\n");
 	kprintf("error : %s\n", error);
 	if (fault) kprintf("code : 0x%lx\n", fault->err_code);
 	kprintf("========================= INFO =========================\n");
-	kprintf("pid : %ld\tstack top : 0x%p\n", pid, stack_top);
+	kprintf("tid : %ld\tstack top : 0x%p\n", pid, stack_top);
 
 	if (fault) {
 		arch_registers_dump(fault);
 	} else {
 		kprintf("==================== REGISTERS DUMP ====================\n");
-		kprintf("unavalible\n");
+		kprintf("unavailable\n");
 	}
 	arch_registers_stacktrace(fault);
 
