0bec7b551472d59ecf841346ae92201b3255d5ed
tayoky <tayoky4848@gmail.com>
2026-08-25 11:18:31 +0200

    scsi : libscsi : add base libscsi

---
+++ modules/block/scsi/libscsi/libscsi.c
@@ -0,0 +1,137 @@
+#include <kernel/module.h>
+#include <kernel/kheap.h>
+#include <kernel/slab.h>
+#include <kernel/bus.h>
+#include <module/scsi.h>
+
+static slab_cache_t scsi_commands_slab;
+
+static int scsi_submit_or_queue_command(scsi_command_t *command) {
+	int ret = ioreq_submit(&command->ioreq);
+	if (ret == -EAGAIN) {
+		// request cannot be send for the moment,
+		// queue it
+		list_append(&command->device->pending_commands, &command->node);
+		ret = 0;
+	}
+	return ret;
+}
+
+static int scsi_submit_command(ioreq_t *ioreq) {
+	scsi_command_t *scsi_command = container_of(ioreq, scsi_command_t, ioreq);
+	scsi_driver_t *scsi_driver = container_of(scsi_command->device->bus->driver, scsi_driver_t, driver);
+	kdebugf("send ");
+	scsi_print_command(scsi_command);
+	kprintf("\n");
+	return scsi_driver->submit_scsi_command(scsi_command->device->channel, scsi_command->device, scsi_command);
+}
+
+static void scsi_finish_command(ioreq_t *ioreq) {
+	scsi_command_t *scsi_command = container_of(ioreq, scsi_command_t, ioreq);
+	scsi_device_t *device = scsi_command->device;
+
+	// resubmit pending requests
+	// NOTE : we do not need to guarantee that if the request 
+	// is once again made pending, it return to the top of the queue
+	// since this queue is only used for configuration commands
+	if (list_is_empty(&device->pending_commands)) {
+		return;
+	}
+	scsi_command_t *pending_command = container_of(device->pending_commands.first_node, scsi_command_t, node);
+	list_remove(&device->pending_commands, &pending_command->node);
+	int ret = scsi_submit_or_queue_command(pending_command);
+	if (ret < 0) {
+		ioreq_finish(&pending_command->ioreq, ret);
+	}
+}
+
+static void scsi_free_command(ioreq_t *ioreq) {
+	scsi_command_t *scsi_command = container_of(ioreq, scsi_command_t, ioreq);
+	slab_free(scsi_command);
+}
+
+static ioreq_ops_t scsi_command_ops = {
+	.submit  = scsi_submit_command,
+	.finish  = scsi_finish_command,
+	.cleanup = scsi_free_command,
+};
+
+scsi_command_t *scsi_create_command(scsi_device_t *device, void *data, size_t size);
+	scsi_driver_t *scsi_driver = container_of(device->channel->driver, scsi_driver_t, driver);
+	if (!scsi_driver->submit_scsi_command) return NULL;
+	scsi_command_t *command = slab_alloc(&scsi_commands_slab);
+	if (!command) return NULL;
+	memset(command, 0, sizeof(scsi_command_t));
+	command->device = device;
+	command->ioreq.ops = &scsi_command_ops;
+	if (data) {
+		kassert(size <= sizeof(command->data));
+		memcpy(command->data, data, size);
+	}
+	return command;
+}
+
+int scsi_submit_command_sync(scsi_command_t *command) {
+	ioreq_ref(&command->ioreq);
+	int ret = scsi_submit_or_queue_command(command);
+	if (ret >= 0) {
+		ret = ioreq_wait(&command->ioreq);
+	}
+	ioreq_release(&command->ioreq);
+	return ret;
+}
+
+static const char scsi_opcode2str(uint8_t command) {
+#define COMMAND(opcode) case SCSI_CMD_ ## opcode: return #opcode;
+	switch (command & 0x1f) {
+	COMMAND(INQUIRY)
+	default:
+		return "UNKNOWN";
+	}
+#undef COMMAND
+}
+
+void scsi_print_command(scsi_command_t *command) {
+	kprintf("SCSI command ");
+	kprintf("opcode=%02hhx(%s) ", command->regs.command, scsi_opcode2str(command->regs.command));
+	kprintf("flags=%02hhx(", command->flags);
+	int prev = 0;
+#define FLAG(x) \
+	if (command->flag & SCSI_CMD_ ## x) {\
+		if (prev) kprintf(", "); \
+		prev = 1; \
+		kprintf(#x);\
+	}
+	FLAG(READ_BUF)
+	FLAG(WRITE_BUF)
+#undef FLAG
+	kprintf(")");
+}
+
+int libscsi_init(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
+	slab_init(&scsi_commands_slab, sizeof(scsi_command_t), "scsi-commands");
+	EXPORT(scsi_create_command);
+	EXPORT(scsi_submit_command_sync);
+	EXPORT(scsi_parse_common_ident);
+	return 0;
+}
+
+int libscsi_fini(void) {
+	slab_destroy(&scsi_commands_slab);
+	UNEXPORT(scsi_create_command);
+	UNEXPORT(scsi_submit_command_sync);
+	UNEXPORT(scsi_parse_common_ident);
+	return 0;
+}
+
+kmodule_t module_meta = {
+	.magic       = MODULE_MAGIC,
+	.init        = libscsi_init,
+	.fini        = libscsi_fini,
+	.author      = "tayoky",
+	.name        = "libscsi",
+	.description = "common SCSI utils",
+	.license     = "GPL 3",
+};
---
+++ modules/include/module/scsi.h
@@ -0,0 +1,121 @@
+#ifndef MODULE_SCSI_H
+#define MODULE_SCSI_H
+
+#include <kernel/bus.h>
+#include <kernel/ioreq.h>
+#include <stdint.h>
+
+#define SCSI_OPCODE_6BYTE            (0b000 << 5)
+#define SCSI_OPCODE_10BYTE           (0b001 << 5)
+#define SCSI_OPCODE_10BYTE_SECONDARY (0b010 << 5)
+#define SCSI_OPCODE_16BYTE           (0b100 << 5)
+#define SCSI_OPCODE_12BYTE           (0b101 << 5)
+
+typedef struct scsi_generic {
+	uint8_t opcode;
+	uint8_t data[15];
+} scsi_generic_t;
+
+typedef struct scsi_data16 {
+	uint8_t high;
+	uint8_t low;
+} __attribute__((packed)) scsi_data16_t;
+
+typedef struct scsi_inquiry {
+	uint8_t opcode;
+	uint8_t flags;
+	uint8_t page_code;
+	scsi_data16_t allocation_lenght;
+	uint8_t control;
+} __attribute__((packed)) scsi_inquiry_t;
+
+#define SCSI_INQUIRY_EVPD 0x01
+
+typedef struct scsi_inquiry_data {
+	uint8_t peripheral;
+	uint8_t rmb;
+	uint8_t version;
+	uint8_t data_format;
+	uint8_t additional_lenght;
+	uint8_t flags1;
+	uint8_t flags2;
+	uint8_t flags3;
+	uint8_t vendor_ident[8];
+	uint8_t product_ident[8];
+	uint8_t product_revision[8];
+	uint8_t serial[8];
+	uint8_t vendor_unique[8];
+	uint8_t reserved0;
+	uint8_t reserved1;
+	scsi_data16_t version_descriptors[8];
+	uint8_t reserved1[22];
+} __attribute__((packed)) scsi_inquiry_data_t;
+
+#define SCSI_INQUIRY_PERIPHERAL_QUALIFIER              (0b111 << 5)
+#define SCSI_INQUIRY_PERIPHERAL_QUALIFIER_SUPPORTED   (0b000 << 5)
+#define SCSI_INQUIRY_PERIPHERAL_QUALIFIER_UNCONNECTED (0b001 << 5)
+#define SCSI_INQUIRY_PERIPHERAL_QUALIFIER_RESERVED    (0b010 << 5)
+#define SCSI_INQUIRY_PERIPHERAL_QUALIFIER_UNSUPPORTED (0b011 << 5)
+#define SCSI_INQUIRY_PERIPHERAL_TYPE 0x1f
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SBC4    0x00
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SSC3    0x01
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SSC     0x02
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SPC2    0x03
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SBC_ALT 0x04
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_MMC5    0x05
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SBC     0x07
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SMC3    0x08
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SCC2    0x0c
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_SES     0x0d
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_RBC     0x0e
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_OCRW    0x0f
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_BCC     0x10
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_OSD     0x11
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_ADC2    0x12
+#define SCSI_INQUIRY_PERIPHERAL_TYPE_UNKNOWN 0x1f
+
+#define SCSI_INQUIRY_RMB 0x80 // device is removable
+
+#define SCSI_VERSION_NONE 0x00
+#define SCSI_VERSION_SPC  0x03
+#define SCSI_VERSION_SPC2 0x04
+#define SCSI_VERSION_SPC3 0x05
+#define SCSI_VERSION_SPC4 0x06
+#define SCSI_VERSION_SPC5 0x07
+// TODO : SPC6 ??
+
+#define SCSO_VENDOR_IDENT_SEAGATE "SEAGATE "
+
+typedef struct scsi_device {
+	devnode_t devnode;
+	devnode_t *bus;
+	list_t pending_commands;
+} scsi_device_t;
+
+typedef struct scsi_command {
+	ioreq_t ioreq;
+	scsi_generic_t data;
+	list_node_t node;
+	scsi_device_t *device;
+	void *buf;
+	size_t buf_size;
+	uint8_t flags;
+} scsi_command_t;
+
+#define SCSI_CMD_READ_BUF  0x01
+#define SCSI_CMD_WRITE_BUF 0x02
+
+typedef struct scsi_driver {
+	driver_t driver;
+	int (*submit_scsi_command)(devnode_t *channel, scsi_device_t *device, scsi_command_t *command);
+} scsi_driver_t;
+
+scsi_command_t *scsi_create_command(scsi_device_t *device, void *data, size_t size);
+
+int scsi_submit_command_sync(scsi_command_t *command);
+
+void scsi_parse_common_ident(scsi_common_ident_t *common_ident, scsi_ident_t *ident);
+
+void scsi_print_command(scsi_command_t *command);
+
+#endif
