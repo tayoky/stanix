866cf1d100357356187890177563c9f9708f0a7c
tayoky <tayoky4848@gmail.com>
2026-08-31 19:26:10 +0200

    kcmdline : fix strcmp

--- userspace/kcmdline/main.c
+++ userspace/kcmdline/main.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 		}
 
 		for (int i = 1; i < argc; i++) {
-			if (strcmp(arg, argv[i])) {
+			if (!strcmp(arg, argv[i])) {
 				if (value) puts(value);
 				return 0;
 			}
