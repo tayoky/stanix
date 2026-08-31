a9ed4415ff1e662a87b4cce9381f7e8fbdbe4b4b
tayoky <tayoky4848@gmail.com>
2026-08-31 15:40:33 +0200

    kcmdline : parse option arguments

--- userspace/kcmdline/main.c
+++ userspace/kcmdline/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		puts("usage : kcmdline OPTION");
+		puts("usage : kcmdline OPTION...");
 		puts("small utility to check if kernel cmdline has a specfied option");
+		puts("if the option has an argument it is printed to stdout");
 		return 1;
 	}
 
@@ -16,10 +18,22 @@ int main(int argc, char **argv) {
 	char cmdline[256];
 	fgets(cmdline, sizeof(cmdline), cmdline_file);
 
-	for (int i = 1; i < argc; i++) {
-		if (strstr(cmdline, argv[i])) {
-			return 0;
+	char *arg = strtok(cmdline, " ");
+	while (arg) {
+		char *value = strchr(arg, '=');
+		if (value) {
+			*value = '\0';
+			value++;
 		}
+
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(arg, argv[i])) {
+				if (value) puts(value);
+				return 0;
+			}
+		}
+
+		arg = strtok(NULL, " ");
 	}
 
 	return 1;
