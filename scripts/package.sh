d5b0bc5ac1a30ee00e10191a458e81be3834f7b3
tayoky <tayoky4848@gmail.com>
2026-09-19 23:54:02 +0200

    package : make empty directory by default

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,6 +8,7 @@ PACKAGES_DIR="$BUILDDIR/packages"
 for PACKAGE in "$@" ; do
 	PACKAGE_DIR="$PACKAGES_DIR/$PACKAGE"
 	rm -fr "$PACKAGE_DIR"
+	mkdir -p "$PACKAGE_DIR"
 	DESTDIR="$PACKAGE_DIR" "$TINX" --no-deps --reinstall install "$PACKAGE"
 
 	# make the tar
