31a10886cf1425f832796d5424940f9a6c1cd0ee
tayoky <tayoky4848@gmail.com>
2026-09-20 19:28:14 +0200

    nyancat : fix package

--- host-packages/nyancat.sh
+++ host-packages/nyancat.sh
@@ -1,3 +1,4 @@
+VERSION="1.5.2"
 SOURCE="nyancat"
 DEPENDENCIES="stanix-base"
 
@@ -6,10 +7,12 @@ configure () {
 }
 
 build () {
-    make -C "$SOURCE_DIR"
+    make -C "$SOURCE_DIR" CC="$HOST-gcc"
 }
 
 install () {
     mkdir -p "$DESTDIR$PREFIX/bin"
-	cp -p "src/nyancat" "$DESTDIR$PREFIX/bin"
+    mkdir -p "$DESTDIR$PREFIX/share/man1"
+	cp -p "$SOURCE_DIR/src/nyancat" "$DESTDIR$PREFIX/bin/"
+	cp -p "$SOURCE_DIR/nyancat.1" "$DESTDIR$PREFIX/share/man1/"
 }
--- sources/nyancat/nyancat.sh
+++ sources/nyancat/nyancat.sh
@@ -1,2 +1,2 @@
 GIT="https://github.com/klange/nyancat"
-COMMIT="32fd2eb40332ae0001995705f0c1f8de69a2d543"
+TAG="$VERSION"
