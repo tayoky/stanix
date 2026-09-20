7bbdfdc2bab58da2cb532e3769d2a127e210e040
tayoky <tayoky4848@gmail.com>
2026-09-20 19:36:18 +0200

    build-hdd-image : change disk sizes and fix limine install

--- scripts/build-hdd-image.sh
+++ scripts/build-hdd-image.sh
@@ -9,8 +9,8 @@ mkdir -p "$IMAGE_DIR"
 "$TOP/scripts/build-esp.sh"
 
 : ${HEAD_SIZE:=1}
-: ${ESP_SIZE:=10}
-: ${EXT2_SIZE:=32}
+: ${ESP_SIZE:=128}
+: ${EXT2_SIZE:=128}
 
 : ${ESP_ROOT:="$BUILDDIR/esp"}
 : ${EXT2_ROOT:="$SYSROOT"}
@@ -32,4 +32,4 @@ cat "$HEAD_IMAGE" "$ESP_IMAGE" "$EXT2_IMAGE" > "$HDD_IMAGE"
 sgdisk -n 1:2048:+${ESP_SIZE}M -t 1:ef00 -c 1:"EFI System Partition" "$HDD_IMAGE"
 sgdisk -n 2:0:0 -t 2:8300 -c 2:"Stanix" "$HDD_IMAGE"
 
-limine install-bios "$HDD_IMAGE"
+limine bios-install "$HDD_IMAGE"
