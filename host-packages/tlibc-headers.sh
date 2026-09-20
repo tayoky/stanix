81b091997d4c35fe427bde549f760d2f66df6913
tayoky <tayoky4848@gmail.com>
2026-09-20 19:41:05 +0200

    tlibc-headers : configure with fake compiler

--- host-packages/tlibc-headers.sh
+++ host-packages/tlibc-headers.sh
@@ -5,6 +5,12 @@ SOURCE="tlibc"
 # install first party packages in /usr
 PREFIX=/usr
 
+configure () {
+	# give a fake compiler since the real compiler might not be ready yet
+	"$SOURCE_DIR/configure" --builddir="$BUILD_DIR" --host="$HOST" --prefix="$PREFIX" \
+		--cc=true --as=true --ar=true
+}
+
 build () {
 	make all-include -j"$PARALLELISM"
 }
