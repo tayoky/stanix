a90da4402f4927f2b12a9e7eae6d803f2cbd4f3c
tayoky <tayoky4848@gmail.com>
2026-09-25 19:17:53 +0200

    download-packages : add script to download packages

---
+++ scripts/download-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+BASE_URL="https://github.com/tayoky/packages/releases/download/${UPLOAD_RELEASE:-"stable"}"
+DOWNLOAD_DIR="$BUILDDIR/downloads"
+mkdir -p "$DOWNLOAD_DIR"
+
+for PACKAGE in "$@" ; do
+	TAR_NAME="$PACKAGE.tar.gz"
+	echo "$BASE_URL/$TAR_NAME"
+	curl -fL -o "$DOWNLOAD_DIR/$TAR_NAME" "$BASE_URL/$TAR_NAME"
+
+	# unpack on sysroot
+	tar -xf "$DOWNLOAD_DIR/$TAR_NAME" -C "$SYSROOT"
+done
